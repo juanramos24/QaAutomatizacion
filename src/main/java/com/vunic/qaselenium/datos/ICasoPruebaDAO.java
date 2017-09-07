4ecdc9563778d17af70e18bb932db7ae2350ced8
pavilesg <pavilesg@PC-PAVILESG>
2017-09-07 11:38:27 -0300

    interfaces nuevas

---
+++ src/main/java/com/vunic/qaselenium/datos/ICasoPruebaDAO.java
@@ -0,0 +1,8 @@
+package com.vunic.qaselenium.datos;
+
+import com.vunic.core.base.IGenericDAO;
+import com.vunic.qaselenium.dto.CategoriaDTO;
+
+public interface ICasoPruebaDAO extends IGenericDAO<CategoriaDTO, Integer> {
+
+}
---
+++ src/main/java/com/vunic/qaselenium/datos/ICategoriaDAO.java
@@ -0,0 +1,8 @@
+package com.vunic.qaselenium.datos;
+
+import com.vunic.core.base.IGenericDAO;
+import com.vunic.qaselenium.dto.CategoriaDTO;
+
+public interface ICategoriaDAO extends IGenericDAO<CategoriaDTO, Integer> {
+
+}
